37a9f979430bdf20619a88797320d84f7549416c
Daniel Hertz <git@dhertz.com>
2016-06-06 14:47:08 +0100

    Fix formatting, add diagram and move home/end

--- keyboard/hhkb/keymap_dan.c
+++ keyboard/hhkb/keymap_dan.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Dan's HHKB JP Layout
  */
 #include "keymap_common.h"
@@ -10,12 +10,24 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] __attribute__ ((section (".key
 #else
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
-    /* Layer 0: Default Layer */
-    KEYMAP_DAN(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, DEL,BSPC, \
-              TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC, \
-              LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NUHS,ENT, \
-              LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RO,  UP,  RSFT, \
-              FN0, ZKHK,LGUI,LALT,F16,     SPC,      RALT,RGUI,RCTL,FN0, LEFT,DOWN,RGHT),
+    /* Layer 0: Default Layer
+     * ,-----------------------------------------------------------.
+     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Del|Bsp|
+     * |-----------------------------------------------------------|
+     * |Tab |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Enter |
+     * |------------------------------------------------------`    |
+     * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  \|    |
+     * |-----------------------------------------------------------|
+     * |Shift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|   |Up |Shi|
+     * |-----------------------------------------------------------|
+     * |Fn0||  #|Gui|Alt|F16|  Space  |Alt|GUI|Con|Fn0||Rig|Dow|Lef|
+     * `-----------------------------------------------------------'
+     */
+    KEYMAP_DAN(ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL, DEL,BSPC,  \
+              TAB    ,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,         \
+              LCTL    ,   A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN,QUOT,NUHS, ENT ,  \
+              LSFT     ,   Z,   X,   C,   V,   B,   N,   M,COMM, DOT, SLSH,  RO,  UP,RSFT, \
+              FN0 , ZKHK,LGUI,LALT, F16,    SPC ,     RALT,RGUI,RCTL, FN0, LEFT,DOWN,RGHT),
 
     /* Layer 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
@@ -23,18 +35,18 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |-----------------------------------------------------------|
      * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |     |
      * |------------------------------------------------------`    |
-     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|   |    |
+     * |      |VoD|VoU|Mut|   |   |  *|  /|   |   |Lef|Rig|   |    |
      * |-----------------------------------------------------------|
-     * |       |   |   |   |   |   |  +|  -|End|PgD|Dow|   |   |   |
+     * |       |   |   |   |   |   |  +|  -|   |   |Dow|   |PgU|   |
      * |-----------------------------------------------------------|
-     * |   ||   |   |   |   |         |   |   |   |   ||   |   |   |
+     * |   ||   |   |   |   |         |   |   |   |   ||Hom|PgD|End|
      * `-----------------------------------------------------------'
-     */ 
-    KEYMAP_DAN(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
-              CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, \
-              TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,PENT, \
-              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,TRNS, \
-              TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+     */
+    KEYMAP_DAN(PWR,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, INS, DEL, \
+              CAPS  ,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,  UP, TRNS,        \
+              TRNS    ,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,TRNS,TRNS,LEFT,RGHT,TRNS,PENT , \
+              TRNS     ,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,TRNS,TRNS,DOWN,TRNS,PGUP,TRNS, \
+              TRNS, TRNS,TRNS,TRNS,TRNS,    TRNS,    TRNS,TRNS,TRNS,TRNS, HOME,PGDN, END),
 };
 
 
